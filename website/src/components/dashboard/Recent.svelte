42f62d3c5c4fe7a915f68a32f889e672fbb8b53a
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-04-18 06:57:11 -0600

    Added dashboard

---
+++ website/src/components/dashboard/Recent.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  const RECENT_PROJECTS = [
+    ["SCII", 3],
+    ["BotComet", 6],
+    ["Circe", 1]
+  ]
+</script>
+
+<div class="
+  flex flex-col justify-start items-start
+  w-fit
+">
+
+  <h3 class="
+    text-lg
+  ">
+    Recent
+  </h3>
+
+  <hr class="
+    w-full
+    mb-2
+  " />
+
+  <ul class="
+    flex flex-col
+    space-y-2
+  ">
+    {#each RECENT_PROJECTS as [project, stars]}
+      <li class="
+        text-lg
+        px-4 py-2
+        rounded-lg
+        bg-gray-600
+        transition-colors
+        flex flex-row justify-between items-center
+        gap-x-8
+      ">
+        {project}
+        <span>
+          ★ {stars}
+        </span>
+      </li>
+    {/each}
+  </ul>
+
+</div>
---
+++ website/src/routes/(with_header)/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Recent from "$components/dashboard/Recent.svelte";
+</script>
+
+<h2 class="
+  text-4xl font-bold
+  mt-16
+">
+  Welcome!
+</h2>
+
+<div class="
+  flex flex-row justify-start items-start
+  w-full flex-grow
+  p-8
+">
+  <Recent />
+</div>
